5ff2037efc063d6fcfdaea4853410e12f8979a87
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2020-12-29 23:55:40 -0300

    finished path, method, queryString and headers parsing

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
 import http from 'http';
+import url from 'url';
 
 
 const server = http.createServer((request, response) => {
-  response.end('Hello, world!\n')
+  const parsedUrl = url.parse(request.url, true);
+
+  const path = parsedUrl.pathname;
+  const trimmedPath = path?.replace(/^\/+|\/+$/g, '');
+  const method = request.method?.toLocaleLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headers = request.headers;
+
+
+  response.end('Hello, world!\n');
+
+  console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these parameters`, queryStringObject);
 });
 
 server.listen(3000, () => {
-  console.log(`The server is listening on port 3000 now`)
-})
+  console.log(`The server is listening on port 3000 now`);
+});
